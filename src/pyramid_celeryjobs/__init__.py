0b2fc932154c98caf030a353b266d66ed5d1476a
James Li <j.li7@griffith.edu.au>
2020-02-21 13:48:35 +1000

    Add CORS support

--- src/pyramid_celeryjobs/__init__.py
+++ src/pyramid_celeryjobs/__init__.py
@@ -7,6 +7,11 @@ def configure(config, ini_location):
 
 def includeme(config):
     settings = config.get_settings()
+
+    # Add CORS support
+    config.include('pyramid_cors')
+    config.add_cors_preflight_handler()
+
     # TODO: load models ... probably won't need it anyway
     # import pyramid_celeryjobs.models  # noqa
     config.add_directive("configure_celery", configure)
